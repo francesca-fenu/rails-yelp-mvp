714788589f1415fd1443a837333b9990e84d9859
Francesca Fenu <fenufrancesca@yahoo.de>
2021-05-19 15:15:19 +0200

    ugly yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
